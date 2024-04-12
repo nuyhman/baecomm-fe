694ef5dbf5a73346bf04e7bc60bad5faabf2d9fa
hohokie <nhpunch@gmail.com>
2024-04-12 17:25:22 +0900

    feat: Updated App component structure - Include ErrorBoundary, Layout, GlobalStyle, and Router in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ErrorBoundary from './components/error/ErrorBoundary';
+import Layout from './components/layout';
+import GlobalStyle from './components/common/GlobalStyle';
+import Router from './components/common/Router';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ErrorBoundary>
+      <Layout>
+        <GlobalStyle />
+        <Router />
+      </Layout>
+    </ErrorBoundary>
   );
 }
 
